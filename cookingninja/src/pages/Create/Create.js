247d14ba91f817c4da2a854626254c61eeb6ccdb
ahmedSadoun <ahmedsaadoun866@gmail.com>
2022-10-08 16:13:56 +0200

    end of create recipe opertation

--- cookingninja/src/pages/Create/Create.js
+++ cookingninja/src/pages/Create/Create.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Create.css";
 export default function Create() {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [newIngredients, setNewIngredients] = useState([]);
+  // this will give me an access to the dom element to use its functionality like now
+  const ingredientInput = useRef(null);
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, newIngredients);
   };
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+    if (ing && !newIngredients.includes(ing)) {
+      setNewIngredients((prevIngredients) => {
+        return [...prevIngredients, ing];
+      });
+    }
+    setNewIngredient("");
+    ingredientInput.current.focus();
+  };
+
   return (
     <div className="create">
       <h2 className="page-title">Add a New Recipe</h2>
@@ -22,6 +38,26 @@ export default function Create() {
             required
           ></input>
         </label>
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            ></input>
+            <button onClick={handleAdd} className="btn">
+              Add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{" "}
+          {newIngredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
         <label>
           <span>Recipe Method</span>
           <textarea
